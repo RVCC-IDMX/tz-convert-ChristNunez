4ca11bb96c711d04f943e048ac311c1a09c95ca9
ChristNunez <christnunez6@gmail.com>
2023-10-23 00:03:37 -0400

    TZ converter

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ESLint Configuration
+# The Time Zone Converter
+The app is meant to find what the year day hour is in the timezone provided by the user
--- tz.js
+++ tz.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-undef */
 /* eslint-disable eqeqeq */
 const moment = require('moment-timezone');
 
+const yargs = require('yargs');
+
 moment.tz.setDefault('America/New_York');
-let targetTimezone;
+
+// let targetTimezone;
 if (process.argv.length != 3) {
   console.log('Usage: node <script-file> <tz>');
   process.exit(1);
-} else {
-  // eslint-disable-next-line prefer-destructuring
-  targetTimezone = process.argv[2];
 }
+// eslint-disable-next-line prefer-destructuring
+const targetTimezone = argv.timezone;
+const useFormattedString = argv.format;
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+if (useFormattedString) {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+} else {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+}
